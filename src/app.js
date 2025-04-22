f64198e68817d426b63d34d037b338a50a810bab
saeed <saeedj808@gmail.com>
2025-04-22 18:04:11 +0330

    add end of the game and edit start

--- src/app.js
+++ src/app.js
@@ -40,6 +40,11 @@ let menu = document.querySelector('#menu');
 let question = document.querySelector('#question');
 let competition = document.querySelector('#competition');
 let options = document.querySelectorAll('.option');
+let ready = document.querySelector('#ready');
+let result = document.querySelector('#result');
+let tryAgain = document.querySelector('#try-again');
+let finalAnswers = document.querySelector('#final-answers');
+let finalTime = document.querySelector('#final-time');
 
 let currentAnswers = document.querySelector("#current-answers");
 let totalQuest = document.querySelector("#total-quest");
@@ -52,22 +57,6 @@ let timer = document.querySelector("#timer");
 let timerInterval;
 let seconds = 0;
 
-function startGame() {
-    questNum = 0
-    storeAnswers = {
-        current: 0,
-    }
-    seconds = 0;
-    fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        updateMenu(questNum, data);
-    })
-    .catch(error => console.error('Error fetching JSON data:', error));
-
-    clearInterval(timerInterval);
-    startTimer();
-}
 
 function startTimer() {
     // setInterval(() => {
@@ -84,51 +73,83 @@ function startTimer() {
     }, 1000);
 }
 
-// startTimer();
+function startGame() {
+    questNum = 0
+    storeAnswers = {
+        current: 0,
+    }
+    seconds = 0;
+    currentAnswers.textContent = storeAnswers.current;
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        updateMenu(questNum, data);
+    })
+    .catch(error => console.error('Error fetching JSON data:', error));
+
+    clearInterval(timerInterval);
+    startTimer();
+}
 
 startButton.addEventListener("click", (event) => {
     menu.style.display = "none";
-    competition.style.display = "block";
-    startGame();
-})
+    ready.style.display = "block";
+    setTimeout(() => {
+        startGame();
+        competition.style.display = "block";
+        ready.style.display = "none";
+    }, 3000);
+});
 
-function updateMenu(questNum, data) {
-    const dataSize = Object.keys(data).length;
-    totalQuest.textContent = `${questNum + 1} of ${dataSize}`;
-    if (data[questNum] && questNum <= dataSize) {
-        question.textContent = data[questNum]["question"];
-        options.forEach((option, index) => {
-            option.textContent = data[questNum]["options"][index];
-            option.onclick = () => {
-                options[data[questNum]["answer"] - 1].style.border = '10px solid rgb(37, 255, 17)';
-                if (data[questNum]["answer"] != index + 1) { // Wrong answer
-                    option.style.border = '10px solid rgb(255, 65, 17)';
-                } else { // Current answer
-                    storeAnswers.current ++
-                    currentAnswers.textContent = storeAnswers.current;
-                }
-                options.forEach(opt => opt.style.pointerEvents = 'none'); // Disable clicks
-                setTimeout(() => {
-                    questNum++;
-                    // update questions and options
-                    question.textContent = data[questNum]["question"];
-                    options.forEach((option, index) => { 
-                        option.textContent = data[questNum]["options"][index];
-                        option.style.border = '10px solid rgb(251, 255, 17)';
-                    });
-                    // console.log(`You selected: ${data[questNum]["options"][index]}`);
-                    if (data[questNum]) {
-                        updateMenu(questNum, data);
-                    } else {
-                        console.log("End of quest.");
-                    }
-                    options.forEach(opt => opt.style.pointerEvents = 'auto'); // Re-enable clicks
-                }, 3000);
-            };
-        });
-    }
+tryAgain.addEventListener("click", (event) => {
+    menu.style.display = "none";
+    ready.style.display = "block";
+    result.style.display = "none"
+    setTimeout(() => {
+        startGame();
+        competition.style.display = "block";
+        ready.style.display = "none";
+    }, 3000);
+});
+
+// function updateMenu(questNum, data) {
+//     const dataSize = Object.keys(data).length;
+//     totalQuest.textContent = `${questNum + 1} of ${dataSize}`;
+//     if (data[questNum] && questNum <= dataSize) {
+//         question.textContent = data[questNum]["question"];
+//         options.forEach((option, index) => {
+//             option.textContent = data[questNum]["options"][index];
+//             option.onclick = () => {
+//                 options[data[questNum]["answer"] - 1].style.border = '10px solid rgb(37, 255, 17)';
+//                 if (data[questNum]["answer"] != index + 1) { // Wrong answer
+//                     option.style.border = '10px solid rgb(255, 65, 17)';
+//                 } else { // Current answer
+//                     storeAnswers.current ++
+//                     currentAnswers.textContent = storeAnswers.current;
+//                 }
+//                 options.forEach(opt => opt.style.pointerEvents = 'none'); // Disable clicks
+//                 setTimeout(() => {
+//                     questNum++;
+//                     // update questions and options
+//                     question.textContent = data[questNum]["question"];
+//                     options.forEach((option, index) => { 
+//                         option.textContent = data[questNum]["options"][index];
+//                         option.style.border = '10px solid rgb(251, 255, 17)';
+//                     });
+//                     // console.log(`You selected: ${data[questNum]["options"][index]}`);
+//                     if (data[questNum]) {
+//                         updateMenu(questNum, data);
+//                     } else {
+//                         console.log("End of quest.");
+                        
+//                     }
+//                     options.forEach(opt => opt.style.pointerEvents = 'auto'); // Re-enable clicks
+//                 }, 3000);
+//             };
+//         });
+//     }
     
-}
+// }
 
 
 function updateMenu(questNum, data) {
@@ -159,8 +180,13 @@ function updateMenu(questNum, data) {
                     // });
                     if (data[questNum]) { // Check again before updating
                         updateMenu(questNum, data);
-                    } else {
+                    } else { // End game
                         console.log("End of quest.");
+                        window.parent.postMessage({ type: "gameCompleted" }, "*");
+                        result.style.display = "grid"
+                        competition.style.display = "none";
+                        finalTime.textContent = timer.textContent
+                        finalAnswers.textContent = storeAnswers.current
                     }
                     options.forEach(opt => {opt.style.pointerEvents = 'auto'; opt.style.border = '10px solid rgb(251, 255, 17)';}); // Re-enable clicks
                 }, 3000);
---
+++ src/main.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('fullscreen-btn');
+const iframe = document.querySelector('iframe');
+
+button.addEventListener('click', () => {
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari, Opera
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+        iframe.msRequestFullscreen();
+    }
+});
+
+// Get message
+window.addEventListener('message', (event) => {
+    // Ensure the message is coming from the expected origin
+    if (event.origin !== window.location.origin) {
+    console.warn('Origin mismatch:', event.origin);
+    return;
+    }
+
+    // Handle the message received from the iframe
+    console.log('Message from iframe:', event.data);
+});
